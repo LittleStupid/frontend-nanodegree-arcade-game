5073dc6e1d480a1a7f56ec2b797039e88d92c8f6
LittleStupid <lttlstpd@live.com>
2016-05-20 12:51:19 +0800

    fix error

--- js/app.js
+++ js/app.js
@@ -86,7 +86,7 @@ Player.prototype.handleInput = function( key ) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = []];
+var allEnemies = [];
 allEnemies.push( new Enemy( 0, 60, 31 ) );
 allEnemies.push( new Enemy( 0, 60, 73 ) );
 
